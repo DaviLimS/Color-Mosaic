57532363635e7550b7e0de099c589af07e64bdf1
DaviLim <95308371+DaviLimS@users.noreply.github.com>
2025-03-03 20:28:55 -0300

    dshs

--- script/script.js
+++ script/script.js
@@ -2,8 +2,8 @@ function GenerateColor(min, max) {
     let red = Math.round((Math.random() * (max - min) + min) * 100);
     let green = Math.round((Math.random() * (max - min) + min) * 100);
     let blue = Math.round((Math.random() * (max - min) + min) * 100);
-
-    let color = red + ", " + green + "," + blue;
-    document.getElementByClassName("color-code").innerText = "color";
-    document.getElementByClassName("color-display").style.backgroundColor = "rgb(" + color + ")";
+    let color = "rgb(" + red + ", " + green + "," + blue + ")";
+    
+    document.getElementByClassName("color-code").innerText = color;
+    document.getElementByClassName("color-display").style.backgroundColor = color;
 }
