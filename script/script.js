275e0da77564d060e811d9bda5c5493506ffbae9
DaviLim <95308371+DaviLimS@users.noreply.github.com>
2025-03-03 21:01:29 -0300

    final

--- script/script.js
+++ script/script.js
@@ -2,8 +2,11 @@ function generateColor(min, max) {
     let red = Math.round((Math.random() * (max - min) + min) * 100);
     let green = Math.round((Math.random() * (max - min) + min) * 100);
     let blue = Math.round((Math.random() * (max - min) + min) * 100);
-    let color = "rgb(" + red + ", " + green + ", " + blue + ")";
+    let color = `rgb(${red}, ${green}, ${blue})`;
 
-    document.getElementsByClassName("color-code").innerText = color;
-    document.getElementsByClassName("color-display").style.backgroundColor = color;
+    let colorCode = document.querySelector(".color-code");
+    let colorDisplay = document.querySelector(".color-display");
+
+    colorCode.innerText = color;
+    colorDisplay.style.backgroundColor = color;
 }
