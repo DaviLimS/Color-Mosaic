aaaa4f1c6939feb1c230dc3318646657595ca8cc
DaviLim <95308371+DaviLimS@users.noreply.github.com>
2025-03-03 21:04:38 -0300

    test

--- script/script.js
+++ script/script.js
@@ -1,7 +1,7 @@
 function generateColor(min, max) {
-    let red = Math.round((Math.random() * (max - min) + min) * 100);
-    let green = Math.round((Math.random() * (max - min) + min) * 100);
-    let blue = Math.round((Math.random() * (max - min) + min) * 100);
+    let red = Math.round((Math.random() * (max - min) + min) * 1000);
+    let green = Math.round((Math.random() * (max - min) + min) * 1000);
+    let blue = Math.round((Math.random() * (max - min) + min) * 1000);
     let color = `rgb(${red}, ${green}, ${blue})`;
 
     let colorCode = document.querySelector(".color-code");
