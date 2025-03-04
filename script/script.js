0e60bf0bcfb06a6d243378cb5cc65459ad18bb83
DaviLim <95308371+DaviLimS@users.noreply.github.com>
2025-03-03 21:34:27 -0300

    haha

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+function typeEffect(element, text, speed) {
+    let i = 0;
+    element.innerText = "";
+    function typing() {
+        if (i < text.lenght) {
+            element.innerText += text.chartAt(i);
+            i++;
+            setTimeout(typing, speed);
+        }
+    }
+    typing();
+}
+
 function generateColor(min, max) {
     let red = Math.round((Math.random() * (max - min) + min) * 1000);
     let green = Math.round((Math.random() * (max - min) + min) * 1000);
@@ -7,6 +20,30 @@ function generateColor(min, max) {
     let colorCode = document.querySelector(".color-code");
     let colorDisplay = document.querySelector(".color-display");
 
-    colorCode.innerText = color;
-    colorDisplay.style.backgroundColor = color;
+    if (colorCode) {
+        typeEffect(colorCodeElement, color, 50);
+    }
+
+    if (colorDisplay) {
+        colorDisplay.style.backgroundColor = color;
+    }
 }
+
+let lastClickTime = 0;
+let clickSpeedLimit = 300;
+let clickNum = 0;
+
+document.querySelector(".color-generator").addEventListener("click", function () {
+    let currentTime = new Date().getTime();
+    if(currentTime - lastClickTime < clickSpeedLimit) {
+        clickNum++;
+    }
+    else {
+        clickNum = 0;
+    }
+    lastClickTime = currentTime;
+
+    if(clickNum == 10) {
+        alert("Calma bb, pqq vc tá clicando tão rápido?? Tá com raiva tá? kkkkkkkkkkkkkkk");
+    }
+})
